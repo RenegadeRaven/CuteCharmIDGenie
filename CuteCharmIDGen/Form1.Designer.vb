994bc4c229e585a72696bae15e346649d08e9289
Regnum <nkochen@hotmail.com>
2019-01-23 00:33:00 -0600

    Initial

---
+++ CuteCharmIDGen/Form1.Designer.vb
@@ -0,0 +1,369 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.gG = New System.Windows.Forms.GroupBox()
+        Me.rSS = New System.Windows.Forms.RadioButton()
+        Me.rHG = New System.Windows.Forms.RadioButton()
+        Me.rPt = New System.Windows.Forms.RadioButton()
+        Me.rP = New System.Windows.Forms.RadioButton()
+        Me.rD = New System.Windows.Forms.RadioButton()
+        Me.gSG = New System.Windows.Forms.GroupBox()
+        Me.sg4 = New System.Windows.Forms.RadioButton()
+        Me.sg3 = New System.Windows.Forms.RadioButton()
+        Me.sg2 = New System.Windows.Forms.RadioButton()
+        Me.sg1 = New System.Windows.Forms.RadioButton()
+        Me.pg4 = New System.Windows.Forms.PictureBox()
+        Me.pg3 = New System.Windows.Forms.PictureBox()
+        Me.pg2 = New System.Windows.Forms.PictureBox()
+        Me.pg1 = New System.Windows.Forms.PictureBox()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.rC = New System.Windows.Forms.RadioButton()
+        Me.rRQ = New System.Windows.Forms.RadioButton()
+        Me.rR = New System.Windows.Forms.RadioButton()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.bGO = New System.Windows.Forms.Button()
+        Me.AR = New System.Windows.Forms.RichTextBox()
+        Me.TID = New System.Windows.Forms.Label()
+        Me.SID = New System.Windows.Forms.Label()
+        Me.gG.SuspendLayout()
+        Me.gSG.SuspendLayout()
+        CType(Me.pg4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pg3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pg2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pg1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'gG
+        '
+        Me.gG.Controls.Add(Me.rSS)
+        Me.gG.Controls.Add(Me.rHG)
+        Me.gG.Controls.Add(Me.rPt)
+        Me.gG.Controls.Add(Me.rP)
+        Me.gG.Controls.Add(Me.rD)
+        Me.gG.Location = New System.Drawing.Point(13, 22)
+        Me.gG.Name = "gG"
+        Me.gG.Size = New System.Drawing.Size(373, 49)
+        Me.gG.TabIndex = 0
+        Me.gG.TabStop = False
+        Me.gG.Text = "Game"
+        '
+        'rSS
+        '
+        Me.rSS.AutoSize = True
+        Me.rSS.Location = New System.Drawing.Point(290, 17)
+        Me.rSS.Name = "rSS"
+        Me.rSS.Size = New System.Drawing.Size(72, 17)
+        Me.rSS.TabIndex = 4
+        Me.rSS.TabStop = True
+        Me.rSS.Text = "SoulSilver"
+        Me.rSS.UseVisualStyleBackColor = True
+        '
+        'rHG
+        '
+        Me.rHG.AutoSize = True
+        Me.rHG.Location = New System.Drawing.Point(211, 17)
+        Me.rHG.Name = "rHG"
+        Me.rHG.Size = New System.Drawing.Size(73, 17)
+        Me.rHG.TabIndex = 3
+        Me.rHG.TabStop = True
+        Me.rHG.Text = "HeartGold"
+        Me.rHG.UseVisualStyleBackColor = True
+        '
+        'rPt
+        '
+        Me.rPt.AutoSize = True
+        Me.rPt.Location = New System.Drawing.Point(140, 17)
+        Me.rPt.Name = "rPt"
+        Me.rPt.Size = New System.Drawing.Size(65, 17)
+        Me.rPt.TabIndex = 2
+        Me.rPt.TabStop = True
+        Me.rPt.Text = "Platinum"
+        Me.rPt.UseVisualStyleBackColor = True
+        '
+        'rP
+        '
+        Me.rP.AutoSize = True
+        Me.rP.Location = New System.Drawing.Point(85, 17)
+        Me.rP.Name = "rP"
+        Me.rP.Size = New System.Drawing.Size(49, 17)
+        Me.rP.TabIndex = 1
+        Me.rP.TabStop = True
+        Me.rP.Text = "Pearl"
+        Me.rP.UseVisualStyleBackColor = True
+        '
+        'rD
+        '
+        Me.rD.AutoSize = True
+        Me.rD.Location = New System.Drawing.Point(12, 17)
+        Me.rD.Name = "rD"
+        Me.rD.Size = New System.Drawing.Size(67, 17)
+        Me.rD.TabIndex = 0
+        Me.rD.TabStop = True
+        Me.rD.Text = "Diamond"
+        Me.rD.UseVisualStyleBackColor = True
+        '
+        'gSG
+        '
+        Me.gSG.Controls.Add(Me.sg4)
+        Me.gSG.Controls.Add(Me.sg3)
+        Me.gSG.Controls.Add(Me.sg2)
+        Me.gSG.Controls.Add(Me.sg1)
+        Me.gSG.Controls.Add(Me.pg4)
+        Me.gSG.Controls.Add(Me.pg3)
+        Me.gSG.Controls.Add(Me.pg2)
+        Me.gSG.Controls.Add(Me.pg1)
+        Me.gSG.Location = New System.Drawing.Point(13, 71)
+        Me.gSG.Name = "gSG"
+        Me.gSG.Size = New System.Drawing.Size(532, 245)
+        Me.gSG.TabIndex = 1
+        Me.gSG.TabStop = False
+        Me.gSG.Text = "Shiny Group"
+        '
+        'sg4
+        '
+        Me.sg4.AutoSize = True
+        Me.sg4.Location = New System.Drawing.Point(457, 17)
+        Me.sg4.Name = "sg4"
+        Me.sg4.Size = New System.Drawing.Size(14, 13)
+        Me.sg4.TabIndex = 8
+        Me.sg4.TabStop = True
+        Me.sg4.UseVisualStyleBackColor = True
+        '
+        'sg3
+        '
+        Me.sg3.AutoSize = True
+        Me.sg3.Location = New System.Drawing.Point(331, 17)
+        Me.sg3.Name = "sg3"
+        Me.sg3.Size = New System.Drawing.Size(14, 13)
+        Me.sg3.TabIndex = 7
+        Me.sg3.TabStop = True
+        Me.sg3.UseVisualStyleBackColor = True
+        '
+        'sg2
+        '
+        Me.sg2.AutoSize = True
+        Me.sg2.Location = New System.Drawing.Point(203, 17)
+        Me.sg2.Name = "sg2"
+        Me.sg2.Size = New System.Drawing.Size(14, 13)
+        Me.sg2.TabIndex = 6
+        Me.sg2.TabStop = True
+        Me.sg2.UseVisualStyleBackColor = True
+        '
+        'sg1
+        '
+        Me.sg1.AutoSize = True
+        Me.sg1.Location = New System.Drawing.Point(70, 17)
+        Me.sg1.Name = "sg1"
+        Me.sg1.Size = New System.Drawing.Size(14, 13)
+        Me.sg1.TabIndex = 5
+        Me.sg1.TabStop = True
+        Me.sg1.UseVisualStyleBackColor = True
+        '
+        'pg4
+        '
+        Me.pg4.BackgroundImage = Global.CuteCharmIDGen.My.Resources.Resources.g4
+        Me.pg4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pg4.Location = New System.Drawing.Point(400, 36)
+        Me.pg4.Name = "pg4"
+        Me.pg4.Size = New System.Drawing.Size(124, 202)
+        Me.pg4.TabIndex = 3
+        Me.pg4.TabStop = False
+        '
+        'pg3
+        '
+        Me.pg3.BackgroundImage = Global.CuteCharmIDGen.My.Resources.Resources.g3
+        Me.pg3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pg3.Location = New System.Drawing.Point(273, 36)
+        Me.pg3.Name = "pg3"
+        Me.pg3.Size = New System.Drawing.Size(128, 202)
+        Me.pg3.TabIndex = 2
+        Me.pg3.TabStop = False
+        '
+        'pg2
+        '
+        Me.pg2.BackgroundImage = Global.CuteCharmIDGen.My.Resources.Resources.g2
+        Me.pg2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pg2.Location = New System.Drawing.Point(146, 36)
+        Me.pg2.Name = "pg2"
+        Me.pg2.Size = New System.Drawing.Size(128, 202)
+        Me.pg2.TabIndex = 1
+        Me.pg2.TabStop = False
+        '
+        'pg1
+        '
+        Me.pg1.BackgroundImage = Global.CuteCharmIDGen.My.Resources.Resources.g1
+        Me.pg1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pg1.Location = New System.Drawing.Point(8, 36)
+        Me.pg1.Name = "pg1"
+        Me.pg1.Size = New System.Drawing.Size(139, 202)
+        Me.pg1.TabIndex = 0
+        Me.pg1.TabStop = False
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.rC)
+        Me.GroupBox1.Controls.Add(Me.rRQ)
+        Me.GroupBox1.Controls.Add(Me.rR)
+        Me.GroupBox1.Location = New System.Drawing.Point(392, 22)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(373, 49)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Shiny Group Selection"
+        '
+        'rC
+        '
+        Me.rC.AutoSize = True
+        Me.rC.Location = New System.Drawing.Point(306, 17)
+        Me.rC.Name = "rC"
+        Me.rC.Size = New System.Drawing.Size(61, 17)
+        Me.rC.TabIndex = 2
+        Me.rC.TabStop = True
+        Me.rC.Text = "Choose"
+        Me.rC.UseVisualStyleBackColor = True
+        '
+        'rRQ
+        '
+        Me.rRQ.AutoSize = True
+        Me.rRQ.Location = New System.Drawing.Point(157, 17)
+        Me.rRQ.Name = "rRQ"
+        Me.rRQ.Size = New System.Drawing.Size(120, 17)
+        Me.rRQ.TabIndex = 1
+        Me.rRQ.TabStop = True
+        Me.rRQ.Text = "Random (w/ Quirky)"
+        Me.rRQ.UseVisualStyleBackColor = True
+        '
+        'rR
+        '
+        Me.rR.AutoSize = True
+        Me.rR.Location = New System.Drawing.Point(12, 17)
+        Me.rR.Name = "rR"
+        Me.rR.Size = New System.Drawing.Size(126, 17)
+        Me.rR.TabIndex = 0
+        Me.rR.TabStop = True
+        Me.rR.Text = "Random (w/o Quirky)"
+        Me.rR.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.AutoSize = True
+        Me.LinkLabel1.Location = New System.Drawing.Point(1, 4)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(116, 13)
+        Me.LinkLabel1.TabIndex = 6
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "New Update Available!"
+        '
+        'bGO
+        '
+        Me.bGO.Location = New System.Drawing.Point(622, 293)
+        Me.bGO.Name = "bGO"
+        Me.bGO.Size = New System.Drawing.Size(75, 23)
+        Me.bGO.TabIndex = 7
+        Me.bGO.Text = "Get IDs"
+        Me.bGO.UseVisualStyleBackColor = True
+        '
+        'AR
+        '
+        Me.AR.Location = New System.Drawing.Point(552, 107)
+        Me.AR.Name = "AR"
+        Me.AR.ReadOnly = True
+        Me.AR.Size = New System.Drawing.Size(213, 173)
+        Me.AR.TabIndex = 8
+        Me.AR.Text = ""
+        '
+        'TID
+        '
+        Me.TID.AutoSize = True
+        Me.TID.Location = New System.Drawing.Point(580, 82)
+        Me.TID.Name = "TID"
+        Me.TID.Size = New System.Drawing.Size(31, 13)
+        Me.TID.TabIndex = 9
+        Me.TID.Text = "TID: "
+        '
+        'SID
+        '
+        Me.SID.AutoSize = True
+        Me.SID.Location = New System.Drawing.Point(657, 82)
+        Me.SID.Name = "SID"
+        Me.SID.Size = New System.Drawing.Size(31, 13)
+        Me.SID.TabIndex = 10
+        Me.SID.Text = "SID: "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(776, 330)
+        Me.Controls.Add(Me.SID)
+        Me.Controls.Add(Me.TID)
+        Me.Controls.Add(Me.AR)
+        Me.Controls.Add(Me.bGO)
+        Me.Controls.Add(Me.LinkLabel1)
+        Me.Controls.Add(Me.gG)
+        Me.Controls.Add(Me.gSG)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.gG.ResumeLayout(False)
+        Me.gG.PerformLayout()
+        Me.gSG.ResumeLayout(False)
+        Me.gSG.PerformLayout()
+        CType(Me.pg4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pg3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pg2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pg1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents gG As GroupBox
+    Friend WithEvents rSS As RadioButton
+    Friend WithEvents rHG As RadioButton
+    Friend WithEvents rPt As RadioButton
+    Friend WithEvents rP As RadioButton
+    Friend WithEvents rD As RadioButton
+    Friend WithEvents gSG As GroupBox
+    Friend WithEvents pg2 As PictureBox
+    Friend WithEvents pg1 As PictureBox
+    Friend WithEvents pg4 As PictureBox
+    Friend WithEvents pg3 As PictureBox
+    Friend WithEvents sg4 As RadioButton
+    Friend WithEvents sg3 As RadioButton
+    Friend WithEvents sg2 As RadioButton
+    Friend WithEvents sg1 As RadioButton
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents rC As RadioButton
+    Friend WithEvents rRQ As RadioButton
+    Friend WithEvents rR As RadioButton
+    Friend WithEvents LinkLabel1 As LinkLabel
+    Friend WithEvents bGO As Button
+    Friend WithEvents AR As RichTextBox
+    Friend WithEvents TID As Label
+    Friend WithEvents SID As Label
+End Class
---
+++ CuteCharmIDGen/Form1.vb
@@ -0,0 +1,215 @@
+﻿Imports System.Threading
+Public Class Form1
+#Region "Variables"
+    Dim Game As SByte = 0 'Game Selection
+    Dim Group As SByte = 0 'Shiny Group Selection
+    Dim rnd(1) As Boolean '{Is Shiny Group random?, With Quirky?}
+    Dim IDs(1) As Short '{TID, SID} as numbers
+    Dim hIDs(1) As String '{TID, SID} as hex
+    Dim code(3) As String 'AR Codes {D, P, Pt, HGSS}
+
+    Dim apppath As String = My.Application.Info.DirectoryPath 'Path to .exe directory
+    Dim res As String = System.IO.Path.GetFullPath(Application.StartupPath & "\..\..\Resources\") 'Path to Project Resources
+    Dim TempPath As String = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) & "\Temp" 'Path to Temp
+#End Region
+
+    'Startup
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        UpdateChk()
+
+    End Sub
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+        drawDE(pg1)
+        drawDE(pg2)
+        drawDE(pg3)
+        drawDE(pg4)
+        gSG.Enabled = False
+        rD.Checked = False
+        rR.PerformClick()
+        TID.Visible = False
+        SID.Visible = False
+        AR.Enabled = False
+    End Sub
+
+    'Checks For Update
+    Private Sub UpdateChk()
+        If System.IO.File.Exists(TempPath & "\vsn.txt") Then
+            System.IO.File.Delete(TempPath & "\vsn.txt")
+        End If
+        If System.IO.File.Exists(TempPath & "\dt.txt") Then
+            System.IO.File.Delete(TempPath & "\dt.txt")
+        End If
+#If DEBUG Then
+        System.IO.File.WriteAllText(apppath & "/version.txt", My.Application.Info.Version.ToString)
+        System.IO.File.WriteAllText(apppath & "/version.json", "{
+" & ControlChars.Quote & "version" & ControlChars.Quote & ": " & ControlChars.Quote & My.Application.Info.Version.ToString & ControlChars.Quote & "
+}")
+        'If My.Computer.Network.IsAvailable Then
+        '    'My.Computer.Network.DownloadFile("LINK HERE: VERSION", TempPath & "\vsn.txt")
+        '    Dim Reader As New IO.StreamReader(TempPath & "\vsn.txt")
+        '    Dim v As String = Reader.ReadToEnd
+        '    Reader.Close()
+        '    System.IO.File.Delete(TempPath & "\vsn.txt")
+        '    If Application.ProductVersion <> v Then
+        'System.IO.File.WriteAllText(res & "/date.txt", (System.DateTime.Today.Year & "/" & System.DateTime.Today.Month & "/" & System.DateTime.Today.Day))
+        '    End If
+        'End If
+        LinkLabel1.Hide()
+#Else
+        System.IO.File.WriteAllText(TempPath & "\date.txt", My.Resources._date)
+        Dim dat As String = System.IO.File.ReadAllText(TempPath & "\date.txt")
+        Me.Text = "Cute Charm Glitch ID Generator (" & dat & ")"
+        If My.Computer.Network.IsAvailable Then
+            My.Computer.Network.DownloadFile("LINK HERE: DATE", TempPath & "\dt.txt")
+            Dim Reader As New IO.StreamReader(TempPath & "\dt.txt")
+            Dim dtt As String = Reader.ReadToEnd
+            Reader.Close()
+            System.IO.File.Delete(TempPath & "\dt.txt")
+            If dat <> dtt Then
+                LinkLabel1.Text = "New Update Available! " & dtt
+                LinkLabel1.Show()
+            Else
+                LinkLabel1.Hide()
+            End If
+        Else
+            LinkLabel1.Hide()
+        End If
+        System.IO.File.Delete(TempPath & "\date.txt")
+#End If
+    End Sub
+
+    'Generate AR Code
+    Private Sub genAR()
+        'Diamond
+        code(0) = ""
+
+        'Pearl
+        code(1) = ""
+
+        'Platinum
+        code(2) = "94000130 FFFB0000
+B2101D40 00000000
+0000008C " & hIDs(1) & hIDs(0) & "
+D2000000 00000000"
+
+        'HeartGold/SoulSilver
+        code(3) = "94000130 FFFB0000
+B2111880 00000000
+00000084 SSSSTTTT
+D2000000 00000000"
+    End Sub
+
+    'Greys out PictureBoxes
+    Private Sub drawDE(ByVal pb As PictureBox)
+        pb.Enabled = False
+        ControlPaint.DrawImageDisabled(pb.CreateGraphics, pb.BackgroundImage, 0, 0, Color.Gray)
+    End Sub
+
+#Region "Specific Shiny Group RadioButtons"
+    'Shiny Group 1
+    Private Sub sg1_CheckedChanged(sender As Object, e As EventArgs) Handles sg1.CheckedChanged
+        If sg1.Checked = True Then
+            Group = 1
+            drawDE(pg2)
+            drawDE(pg3)
+            drawDE(pg4)
+        Else
+            Group = 0
+            pg2.Enabled = True
+            pg3.Enabled = True
+            pg4.Enabled = True
+        End If
+    End Sub
+
+    'Shiny Group 2
+    Private Sub sg2_CheckedChanged(sender As Object, e As EventArgs) Handles sg2.CheckedChanged
+        If sg2.Checked = True Then
+            Group = 2
+            drawDE(pg1)
+            drawDE(pg3)
+            drawDE(pg4)
+        Else
+            Group = 0
+            pg1.Enabled = True
+            pg3.Enabled = True
+            pg4.Enabled = True
+        End If
+    End Sub
+
+    'Shiny Group 3
+    Private Sub sg3_CheckedChanged(sender As Object, e As EventArgs) Handles sg3.CheckedChanged
+        If sg3.Checked = True Then
+            Group = 3
+            drawDE(pg2)
+            drawDE(pg1)
+            drawDE(pg4)
+        Else
+            Group = 0
+            pg2.Enabled = True
+            pg1.Enabled = True
+            pg4.Enabled = True
+        End If
+    End Sub
+
+    'Shiny Group 4
+    Private Sub sg4_CheckedChanged(sender As Object, e As EventArgs) Handles sg4.CheckedChanged
+        If sg4.Checked = True Then
+            Group = 4
+            drawDE(pg2)
+            drawDE(pg3)
+            drawDE(pg1)
+        Else
+            Group = 0
+            pg2.Enabled = True
+            pg3.Enabled = True
+            pg1.Enabled = True
+        End If
+    End Sub
+#End Region
+#Region "Shiny Group Selection"
+    'User Chooses Shiny Group
+    Private Sub rC_CheckedChanged(sender As Object, e As EventArgs) Handles rC.CheckedChanged
+        If rC.Checked = True Then
+            rnd(0) = False
+            gSG.Enabled = True
+            pg1.Enabled = True
+            pg2.Enabled = True
+            pg3.Enabled = True
+            pg4.Enabled = True
+        Else
+            rnd(0) = Nothing
+            gSG.Enabled = False
+            drawDE(pg1)
+            drawDE(pg2)
+            drawDE(pg3)
+            drawDE(pg4)
+        End If
+    End Sub
+
+    'Shiny Group is random without Group 4
+    Private Sub rR_CheckedChanged(sender As Object, e As EventArgs) Handles rR.CheckedChanged
+        If rR.Checked = True Then
+            rnd(0) = True
+            rnd(1) = False
+        Else
+            rnd(0) = Nothing
+            rnd(1) = Nothing
+        End If
+    End Sub
+
+    'Shiny Group is random with Group 4
+    Private Sub rRQ_CheckedChanged(sender As Object, e As EventArgs) Handles rRQ.CheckedChanged
+        If rRQ.Checked = True Then
+            rnd(0) = True
+            rnd(1) = True
+        Else
+            rnd(0) = Nothing
+            rnd(1) = Nothing
+        End If
+    End Sub
+
+
+#End Region
+
+
+End Class
