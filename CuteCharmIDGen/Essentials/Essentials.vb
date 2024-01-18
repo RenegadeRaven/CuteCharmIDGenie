eb165007f6a367de0db03961dfddcc68177c730b
RenegadeRaven <8683689+RenegadeRaven@users.noreply.github.com>
2024-01-18 02:16:41 -0600

    Fixed UpdateCheck()

--- CuteCharmIDGen/Essentials/Essentials.vb
+++ CuteCharmIDGen/Essentials/Essentials.vb
@@ -12,7 +12,7 @@ Module Essentials
 }")
         If My.Computer.Network.IsAvailable And Pinger() Then
             My.Computer.Network.DownloadFile("https://raw.githubusercontent.com/RenegadeRaven/CuteCharmIDGenie/master/CuteCharmIDGen/version.txt", Main.TempPath & "\vsn.txt")
-            Dim v As String = readUpdate(Main.TempPath & "\vsn.txt")
+            Dim v As String = ReadUpdate(Main.TempPath & "\vsn.txt")
             File.Delete(Main.TempPath & "\vsn.txt")
             If Application.ProductVersion <> v Then File.WriteAllText(Main.res & "/date.txt", (System.DateTime.Today.Year & "/" & System.DateTime.Today.Month & "/" & System.DateTime.Today.Day))
         End If
@@ -25,28 +25,16 @@ Module Essentials
             Catch
                 File.WriteAllText(Main.TempPath & "\dt.txt", " ")
             End Try
-            File.Delete(Main.Local & "\updateDate.txt")
             Dim dtt As String = ReadUpdate(Main.TempPath & "\dt.txt")
+            File.Delete(Main.TempPath & "\dt.txt")
             If My.Resources._date <> dtt Then
-                File.Copy(Main.TempPath & "\dt.txt", Main.Local & "\updateDate.txt")
                 Main.lklb_Update.Text = Main.LangRes.GetString("Update") & " " & dtt
                 Main.lklb_Update.Show()
             Else
                 Main.lklb_Update.Hide()
             End If
-            File.Delete(Main.TempPath & "\dt.txt")
         Else
-            If File.Exists(Main.Local & "\updateDate.txt") Then
-                If My.Resources._date <> ReadUpdate(Main.Local & "\updateDate.txt") Then
-                    Main.lklb_Update.Text = Main.LangRes.GetString("Update") & " " & ReadUpdate(Main.Local & "\updateDate.txt")
-                    Main.lklb_Update.Show()
-                Else
-                    File.Delete(Main.Local & "\updateDate.txt")
-                    Main.lklb_Update.Hide()
-                End If
-            Else
-                    Main.lklb_Update.Hide()
-            End If
+            Main.lklb_Update.Hide()
         End If
         File.Delete(Main.TempPath & "\date.txt")
 #End If
